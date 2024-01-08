ee6a33844358908f8169822eea41fe40c0724e0d
KevinW90 <kevinjwilliams76@gmail.com>
2024-01-07 19:42:57 -0500

    transport creation process logic

---
+++ src/lib/systems/transport.ts
@@ -0,0 +1,74 @@
+// all transport logic
+
+export type TransportData = {
+	id: string;
+	status: 'current' | 'completed';
+	monitor: string;
+	coordinator: string;
+};
+
+export class Transport {
+	private _data: TransportData;
+
+	/**
+	 * A transport should be initialized with data.
+	 *
+	 * This data data can come from two sources:
+	 * 1) Database
+	 * 2) Frontend
+	 */
+	constructor(data: TransportData) {
+		this._data = data;
+	}
+
+	/**
+	 * Allows update to any field on the class.
+	 * Ensures field provided exists on the class.
+	 *
+	 * @param field The field to change
+	 * @param value The new value for the field
+	 * @returns Updated class instance
+	 */
+	update<K extends keyof TransportData>(field: K, value: TransportData[K]): Transport {
+		// TODO: set the values in the database
+		this._data[field] = value;
+
+		return this;
+	}
+
+	// getters
+	get id(): string {
+		return this._data.id;
+	}
+
+	get status(): 'current' | 'completed' {
+		return this._data.status;
+	}
+
+	get monitor(): string {
+		return this._data.monitor;
+	}
+
+	get coordinator(): string {
+		return this._data.coordinator;
+	}
+	///////////
+
+	// setters
+	set id(value: TransportData['id']) {
+		this._data.id = value;
+	}
+
+	set status(value: TransportData['status']) {
+		this._data.status = value;
+	}
+
+	set monitor(value: TransportData['monitor']) {
+		this._data.monitor = value;
+	}
+
+	set coordinator(value: TransportData['coordinator']) {
+		this._data.coordinator = value;
+	}
+	///////////
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,89 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Transport, type TransportData } from '$lib/systems/transport';
+
+	let transport: Transport | null = null;
+
+	let adminList = ['Melissa Helgerburth', 'Kenneth Bronstein', 'Angela DeCote'];
+	let searching = false;
+	let searchTimer: ReturnType<typeof setTimeout> | null = null;
+
+	function update(e: Event) {
+		const t = e.target as HTMLSelectElement;
+		const field = t.name as keyof TransportData;
+		const value = (e.target as HTMLSelectElement).value;
+
+		console.log(t.name);
+		transport = transport?.update(field, value) || null;
+
+		if (field === 'id' && value) {
+			searching = true;
+
+			if (searchTimer) clearTimeout(searchTimer);
+
+			searchTimer = setTimeout(() => {
+				routes = Math.random() < 0.5 ? ['1', '2', '3'] : [];
+				searching = false;
+			}, 5000);
+		}
+
+		if (field === 'id' && !value) searching = false;
+	}
+
+	const default_transport: TransportData = {
+		id: '',
+		status: 'current',
+		coordinator: '',
+		monitor: ''
+	};
+
+	let routes: string[] = [];
+</script>
+
+{#if !transport}
+	<div>There is no current transport</div>
+	<button
+		on:click={() => {
+			transport = new Transport(default_transport);
+		}}>New Transport</button
+	>
+{:else}
+	<label>
+		Start Date
+		<select name="id" bind:value={transport.id} on:change={update}>
+			<option value="">Choose a date</option>
+			<option value="01-06-24">01-06-24</option>
+		</select>
+	</label>
+
+	<label>
+		Coordinator
+		<select name="coordinator" bind:value={transport.coordinator} on:change={update}>
+			<option value="">Choose a coordinator</option>
+			{#each adminList as c}
+				<option value={c}>{c}</option>
+			{/each}
+		</select>
+	</label>
+
+	<label>
+		Monitor
+		<select name="monitor" bind:value={transport.monitor} on:change={update}>
+			<option value="">Choose a monitor</option>
+			{#each adminList as c}
+				<option value={c}>{c}</option>
+			{/each}
+		</select>
+	</label>
+
+	{#if searching}
+		<div>Searching for transport: {transport.id}</div>
+	{:else if !transport.id}
+		<div>Choose a date to find the transport data</div>
+	{:else if routes.length === 0}
+		<div>There were no routes found. add passengers</div>
+	{:else}
+		{#each routes as route}
+			<div>{route}</div>
+		{/each}
+	{/if}
+{/if}
